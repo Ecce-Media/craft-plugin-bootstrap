c3823a82bcb9f6aeb2211c687a41bba46dbd8d1b
Carl Glaysher <carl@ecce.uk>
2020-02-21 14:17:08 +0000

    fix preg_replace again!

--- craft/Events.php
+++ craft/Events.php
@@ -98,10 +98,10 @@ class Events
 
 
     protected static function camelCase($string, $dontStrip = []){
-        return lcfirst(str_replace(' ', '', ucfirst(preg_replace('/[^a-z0-9\w'.implode('',$dontStrip).']*/i', ' ',$string))));
+        return lcfirst(str_replace(' ', '', ucfirst(preg_replace('/[^a-z0-9\w'.implode('',$dontStrip).']+/i', ' ',$string))));
     }
     protected static function snakeCase($string, $dontStrip = []){
-        return lcfirst(str_replace(' ', '-', strtolower(preg_replace('/[^a-z0-9\w'.implode('',$dontStrip).']*/i', ' ',$string))));
+        return lcfirst(str_replace(' ', '-', strtolower(preg_replace('/[^a-z0-9\w'.implode('',$dontStrip).']+/i', ' ',$string))));
     }
     protected static function makeDirectories($path, $basePath){
         $path_parts = pathinfo($path);
